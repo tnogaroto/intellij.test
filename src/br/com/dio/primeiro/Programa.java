427b5e4358d6b8a45144bca6ffcfb28b7bc9f1bd
tnogaroto <nogaroto@outlook.com>
2021-09-30 15:07:47 -0300

    criando class livro

--- src/br/com/dio/primeiro/Programa.java
+++ src/br/com/dio/primeiro/Programa.java
@@ -5,14 +5,51 @@ import model.Gato;
 import java.sql.SQLOutput;
 
 public class Programa {
+    private static Livro Livro1;
+
     public static void main(String[] args) {
        Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro1 = new Livro ( nome: "o problema", numPaginas: 300);
+        System.out.println(livro1);
 
 
-        int a =5;
+        /*int a =5;
         int b =3;
-        System.out.println("Hello World!  " + (a+b));
+        System.out.println("Hello World!  " + (a+b));*/
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
